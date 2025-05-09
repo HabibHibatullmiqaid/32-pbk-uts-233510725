6f3ef96b7a07d28c17179da57321e137920446fb
HabibHibatullmiqaid <habibmiqaid96@gmail.com>
2025-05-09 22:24:25 +0700

    menambahkan kegiatan,menandai kegiatan sebagai selesai,menghapus kegiatan

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,7 @@
         <input type="checkbox" v-model="showOnlyIncomplete" />
         Tampilkan hanya yang belum selesai
       </label>
-    </div>
-
+  
     <ul class="task-list">
       <li
         v-for="(task, index) in filteredTasks"
@@ -29,36 +28,10 @@
         <span class="task-text">{{ task.text }}</span>
         <button class="btn-delete" @click="removeTask(index)">❌</button>
       </li>
-    </ul>
+    </ul>  </div>
+
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-
-const newTask = ref('');
-const showOnlyIncomplete = ref(false);
-const tasks = ref([
-  { text: 'Beribadah', done: false },
-  { text: 'Belajar', done: false },
-  { text: 'Berpetualang', done: true },
-  { text: 'Berburu', done: true }, 
-  { text: 'Melestarikan Budaya Lokal', done: true }
-]);
-
-const filteredTasks = computed(() => {
-  return showOnlyIncomplete.value
-    ? tasks.value.filter((task) => !task.done)
-    : tasks.value;
-});
-
-function addTask() {
-  if (newTask.value.trim() === '') return;
-  tasks.value.push({ text: newTask.value, done: false });
-  newTask.value = '';
-}
-
-function removeTask(index) {
-  tasks.value.splice(index, 1);
-}
 </script>
